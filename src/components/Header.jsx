35e07754d3f91b677aa303bba4542a3b06b79c4a
Gabriel Muniz <gabriel.munizz@outlook.com.br>
2022-01-22 00:08:31 -0300

    Requisito 4 finalizado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -31,8 +32,13 @@ class Header extends Component {
         {isLoading
           ? <Loading />
           : (
-            <p data-testid="header-user-name">{userName}</p>
-          ) }
+            <div>
+              <p data-testid="header-user-name">{userName}</p>
+              <Link data-testid="link-to-search" to="/search">Pesquisa</Link>
+              <Link data-testid="link-to-favorites" to="/favorites">Favoritas</Link>
+              <Link data-testid="link-to-profile" to="/profile">Perfil</Link>
+            </div>
+          )}
       </header>
 
     );
