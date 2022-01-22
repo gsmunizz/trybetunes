80d0ed069007ab7014b31173f48770f6051d2468
Gabriel Muniz <gabriel.munizz@outlook.com.br>
2022-01-21 23:29:24 -0300

    Requisito 2 finalizado

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,78 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import Loading from '../components/Loading';
+import { createUser } from '../services/userAPI';
 
 class Login extends Component {
-  render() {
+  constructor() {
+    super();
+
+    this.state = {
+      inputString: '',
+      isLoading: false,
+      isCreated: false,
+    };
+  }
+
+  handleInputChange = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      inputString: value,
+    });
+  }
+
+  isMoreThan = () => {
+    const { inputString } = this.state;
+    const minLength = 3;
     return (
-      <div data-testid="page-login" />
+      inputString.length < minLength
     );
+  }
+
+  handleCreateUser = async () => {
+    const { inputString } = this.state;
+    this.setState({
+      isLoading: true,
+    });
+    await createUser({ name: inputString });
+    this.setState({
+      isCreated: true,
+    });
+  }
+
+  submitNewUser = (isLoading, isCreated, loginForm) => {
+    if (isLoading === true) {
+      if (isCreated === true) {
+        return <Redirect to="/search" />;
+      }
+      return <Loading />;
+    }
+    return loginForm;
+  }
+
+  // Felipe Shinkae me ajudou a completar o requisito.
+
+  render() {
+    const { inputString, isLoading, isCreated } = this.state;
+    const login = (
+      <div data-testid="page-login">
+        <input
+          data-testid="login-name-input"
+          type="text"
+          value={ inputString }
+          onChange={ this.handleInputChange }
+        />
+        <button
+          data-testid="login-submit-button"
+          type="button"
+          disabled={ this.isMoreThan() }
+          onClick={ this.handleCreateUser }
+        >
+          Entrar
+        </button>
+      </div>
+    );
+    return (this.submitNewUser(isLoading, isCreated, login));
   }
 }
 
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class NotFound extends Component {
   render() {
     return (
-      <div data-testid="page-not-found" />
+      <div data-testid="page-not-found">
+        <h1>404 Not Found</h1>
+      </div>
     );
   }
 }
